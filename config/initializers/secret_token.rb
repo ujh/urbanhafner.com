9c598809824800262c1e7f86b75176eef003d16d
Urban Hafner <urban@bettong.net>
2012-08-15 12:23:50 +0200

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrbanhafnerCom::Application.config.secret_token = '90bf5764bf29ae754f035e9498d53f3ab75b4d153e396975664f53fce882e916931c351a8b19202e6eba94cc297cc96ee5207a3e3bc9f6c2900c793f3513bbe6'
