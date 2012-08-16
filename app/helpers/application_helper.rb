9a51d648ead1ca40baea5d7c6f8e493852793d24
Urban Hafner <urban@bettong.net>
2012-08-16 20:37:42 +0200

    Nicer page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
 
+  def title
+    t = "Urban Hafner - Ruby on Rails and JavaScript freelance developer"
+    name = pages.invert[request.path]
+    t = "#{name} | #{t}" if name.present?
+    t
+  end
+
   def copyright_years
     start = 2012
     this_year = Date.today.year
@@ -10,10 +17,19 @@ module ApplicationHelper
     end
   end
 
-  def menu_item(name, link)
+  def menu_item(name, link=pages[name])
     l = link_to_unless_current(name, link) do
       link_to name, link, :class => 'active'
     end
     "<li>#{l}</li>".html_safe
+  end
+  
+  def pages
+    {
+      "Home"     => root_path,
+      "Projects" => projects_path,
+      "Resume"   => resume_path,
+      "Contact"  => contact_path
+    }
   end
 end
